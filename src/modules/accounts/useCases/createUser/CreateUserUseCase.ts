264d14909785904fd6b786ad9ea5a89834e9f5ad
Myltiane Alves <myltiane.aux@gmail.com>
2021-10-07 16:17:47 -0300

    Subindo rotas de autenticação

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,8 +1,10 @@
-import { inject } from 'tsyringe';
+import { inject, injectable } from 'tsyringe';
+import { hash } from 'bcrypt';
 
 import { IUsersRepository } from '../../repositories/IUsersRepository';
 import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
 
+@injectable()
 class CreateUserUseCase{
     constructor(
       @inject("UsersRepository")
@@ -11,16 +13,22 @@ class CreateUserUseCase{
 
     async execute({
         name,
-        username,
         email,
         password,
         driver_license,
     }: ICreateUserDTO): Promise<void> {
+        const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+        if(userAlreadyExists) {
+            throw new Error("User already exists")
+        }
+        
+        const passwordHash = await hash(password, 8);
+
         await this.usersRepository.create({
             name,
-            username,
             email,
-            password,
+            password: passwordHash,
             driver_license,  
         });
     }
