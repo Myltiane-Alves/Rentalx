4f4f37d0c4d996107b3aad286f5c8bf0b89a3097
Myltiane Alves <myltiane.aux@gmail.com>
2021-10-15 11:44:57 -0300

    Refatorando Código

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -1,8 +1,8 @@
 import { compare } from 'bcrypt';
-import { AppError } from '../../../../errors/AppError';
+import { AppError } from '@errors/AppError';
 import { sign } from "jsonwebtoken";
 import { inject, injectable } from "tsyringe";
-import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 
 interface IRequest {
     email: string;
